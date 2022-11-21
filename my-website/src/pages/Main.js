486460d4f208615fbc7f31ab7ddad5f3b689068d
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2022-11-21 01:44:07 -0500

    Update Main.js
    add email

--- my-website/src/pages/Main.js
+++ my-website/src/pages/Main.js
@@ -17,7 +17,9 @@ export default function Main() {
             <div class="letter">e</div>
             <div class="emoji">😊</div>
         </div>
-            <p>I am a 3rd year CS student @ UWaterloo. I'm mainly interested in data science, and
+        <div>
+        </div>
+            <p>I am a 3rd year Computer Science student @ UWaterloo. I'm mainly interested in data science, and
                 my main goal is to bridge the gap between business and technology in today's world!
                 <br></br>
                 <br></br>
@@ -33,6 +35,7 @@ export default function Main() {
                     <li><a href="https://www.facebook.com/josephinewidjaja002/" target="_blank" rel="noreferrer"><i className="fa fa-facebook-square fa-2x"></i></a></li>
                     <li><a href="https://github.com/josephine-w" target="_blank" rel="noreferrer"><i className="fab fa-github fa-2x"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/josephine-w/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in fa-2x"></i></a></li>
+                    <li><a href = "mailto:j4widjaj@uwaterloo.ca" target="_blank" rel="noreferrer"><i className="fa fa-envelope fa-2x"></i></a></li>
                 </ul>
             </div>
         </div>
