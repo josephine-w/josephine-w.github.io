ee6d0b3041196421b572ff2c2d601ec44631e8f8
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2022-11-21 01:44:16 -0500

    Update Experience.js
    add experiences

--- my-website/src/pages/Experience.js
+++ my-website/src/pages/Experience.js
@@ -1,40 +1,32 @@
 import kpmg from '../assets/kpmg.png'
 import bmo from '../assets/bmo.png'
 import assent from '../assets/assent.png'
+import uw from '../assets/uw.png'
 
 export default function Experience() {
     return (
-    <>
-    <div class="experience">
-        <h1>experience 💼</h1>
-        <div class="exp-box">
-            <div class="title">
-                <img src={kpmg}></img>
-                <div class="job">
-                    <h2>DATA SCIENCE INTERN</h2>
-                    <h3>KPMG</h3>
-                </div>
+        <>
+        <div class="experience">
+            <h1>experience 💼</h1>
+            <div class="exp-box">
+                <h2 class="left"><img src={uw} alt="uwaterloo"></img>bachelor's of CS @ <a href="https://cs.uwaterloo.ca/" target="_blank" rel="noreferrer">UWATERLOO</a></h2>
+                <h2 class="right">09/2022 - 12/2022</h2>
+            </div>
+            <hr align="left"></hr>
+            <div class="exp-box">
+                <h2 class="left"><img src={kpmg} alt="kpmg"></img>data science intern @ <a href="https://advisory.kpmg.us/services/lighthouse.html" target="_blank" rel="noreferrer">KPMG</a></h2>
+                <h2 class="right">09/2022 - 12/2022</h2>
+            </div>
+            <div class="exp-box">
+                <h2 class="left"><img src={bmo} alt="bmo"></img>business tech analyst intern @ <a href="https://www.bmo.com/main/personal" target="_blank" rel="noreferrer">BMO</a></h2>
+                <h2 class="right">01/2022 - 04/2022</h2>
+            </div>
+            <div class="exp-box">
+                <h2 class="left"><img src={assent} alt="assent"></img>machine learning intern @ <a href="https://www.assent.com/" target="_blank" rel="noreferrer">ASSENT</a></h2>
+                <h2 class="right">05/2021 - 08/2021</h2>
             </div>
         </div>
-        <div class="exp-box">
-        <div class="title">
-                <img src={bmo}></img>
-                <div class="job">
-                    <h2>BUSINESS TECHNOLOGY ANALYST INTERN</h2>
-                    <h3>Bank of Montreal</h3>
-                </div>
-            </div>
-        </div>
-        <div class="exp-box">
-            <div class="title">
-                <img src={assent}></img>
-                <div class="job">
-                    <h2>MACHINE LEARNING INTERN</h2>
-                    <h3>Assent</h3>
-                </div>
-            </div>
-        </div>
-    </div>
-    </>
+        </>
     )
 }
+
