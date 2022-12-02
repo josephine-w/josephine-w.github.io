4794da7dcafd8817c836bade99ce29cb9910c285
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2022-12-01 20:35:43 -0500

    updates

---
+++ src/Navbar.js
@@ -0,0 +1,39 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import me from './assets/me.png'
+
+export default function Navbar() {
+    return (
+    <nav className="nav">
+        <ul>
+            <ProfileImage to="/"></ProfileImage>
+            <CustomLink to="/experience">experience</CustomLink>
+            <CustomLink to="/projects">projects</CustomLink>
+            <CustomLink to="/art">art</CustomLink>
+        </ul>
+    </nav>
+    )
+}
+
+function ProfileImage({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <Link to={ to }>
+            <img src={me} alt="me" className={isActive ? "active" : ""}/>
+        </Link>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <li>
+            <Link to={ to } className={isActive ? "active" : ""}{...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
+
+
---
+++ src/pages/Main.js
@@ -0,0 +1,46 @@
+import resume from '../assets/resume.pdf'
+import FadeIn from 'react-fade-in';
+
+export default function Main() {
+    return (
+        <>
+            <div className="main">
+            <FadeIn delay="300" transitionDuration="500">
+                <div class="name">
+                    <div class="hello">hi! i'm </div>
+                    <div class="letter">j</div>
+                    <div class="letter">o</div>
+                    <div class="letter">s</div>
+                    <div class="letter">e</div>
+                    <div class="letter">p</div>
+                    <div class="letter">h</div>
+                    <div class="letter">i</div>
+                    <div class="letter">n</div>
+                    <div class="letter">e</div>
+                    <div class="emoji">😊</div>
+                </div>
+                <div className="info">
+                    <p>I am a 3rd year Computer Science student @ UWaterloo. I'm mainly interested in data science, and
+                        my main goal is to bridge the gap between business and technology in today's world!
+                        <br></br>
+                        <br></br>
+                        Currently @ <a href="https://advisory.kpmg.us/services/lighthouse.html" target="_blank" rel="noreferrer">KPMG</a>.
+                        <br></br>
+                        <br></br>
+                        Feel free to contact me!
+                    </p>
+                </div>
+                <div className="socials">
+                        <ul>
+                            <li><a href={resume} target="_blank" rel="noreferrer"><i className="fas fa-file fa-2x"></i></a></li>
+                            <li><a href="https://www.facebook.com/josephinewidjaja002/" target="_blank" rel="noreferrer"><i className="fa fa-facebook-square fa-2x"></i></a></li>
+                            <li><a href="https://github.com/josephine-w" target="_blank" rel="noreferrer"><i className="fab fa-github fa-2x"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/josephine-w/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in fa-2x"></i></a></li>
+                            <li><a href = "mailto:j4widjaj@uwaterloo.ca" target="_blank" rel="noreferrer"><i className="fa fa-envelope fa-2x"></i></a></li>
+                        </ul>
+                </div>
+                </FadeIn>
+            </div>
+        </>
+    )
+}
