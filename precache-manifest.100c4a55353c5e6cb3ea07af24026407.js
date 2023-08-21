df3b59381d85173b98a54c60d0c93fde8988e54f
Tri Ho <vinhocent@gmail.com>
2023-08-20 20:03:24 -0400

    Updates

---
+++ precache-manifest.100c4a55353c5e6cb3ea07af24026407.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e601c8c595939d57d216",
+    "url": "/static/css/main.7657e7ab.chunk.css"
+  },
+  {
+    "revision": "e601c8c595939d57d216",
+    "url": "/static/js/main.9657cdab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "463f07a7702b036bf9d6",
+    "url": "/static/js/2.1ee269d4.chunk.js"
+  },
+  {
+    "revision": "9b269d0e8d4da4acf0b689d42413d145",
+    "url": "/static/media/me.9b269d0e.png"
+  },
+  {
+    "revision": "731d77202ae552183140d1f0cd74c4f5",
+    "url": "/static/media/resume.731d7720.pdf"
+  },
+  {
+    "revision": "9da95b39594bac27c3bcaf76c1d723f5",
+    "url": "/static/media/bmo.9da95b39.png"
+  },
+  {
+    "revision": "cb0ac860078b0d0d1508455fed2f301a",
+    "url": "/static/media/assent.cb0ac860.png"
+  },
+  {
+    "revision": "901917f9b6e74d254525c3e37d3dd934",
+    "url": "/static/media/uw.901917f9.png"
+  },
+  {
+    "revision": "716f087b0d62804af7aa826af8d1ac52",
+    "url": "/static/media/fridgey.716f087b.png"
+  },
+  {
+    "revision": "21030791aca4faa187d20f2ae031003d",
+    "url": "/static/media/operate.21030791.png"
+  },
+  {
+    "revision": "02fe6fbc6a22d4fe6ddeb578e1335bfb",
+    "url": "/static/media/website.02fe6fbc.png"
+  },
+  {
+    "revision": "5a346c6b71e3400564a11f5d47276eb3",
+    "url": "/static/media/weether.5a346c6b.png"
+  },
+  {
+    "revision": "b050be33e475cf8a2117838040678daf",
+    "url": "/static/media/mymelo.b050be33.gif"
+  },
+  {
+    "revision": "ecb9371ebf0be702f7189fbe989e5e7b",
+    "url": "/index.html"
+  }
+];
