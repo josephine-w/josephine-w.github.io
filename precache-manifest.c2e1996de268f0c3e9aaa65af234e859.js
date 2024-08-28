a423ca569191e6dc0d6d2981949db1c8f68379f8
Josephine W <75404368+josephine-w@users.noreply.github.com>
2024-08-28 04:53:49 +0000

    Updates

---
+++ precache-manifest.c2e1996de268f0c3e9aaa65af234e859.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc69ec9bb91d0d755e853633c38b958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc9d9a0d7b7ecc20323",
+    "url": "/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "7d0db33ee1faa1b5ae1b",
+    "url": "/static/css/main.75831d29.chunk.css"
+  },
+  {
+    "revision": "0dc9d9a0d7b7ecc20323",
+    "url": "/static/js/2.1bf07f39.chunk.js"
+  },
+  {
+    "revision": "27276dfec34e2e44d38d12f127eced24",
+    "url": "/static/js/2.1bf07f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0db33ee1faa1b5ae1b",
+    "url": "/static/js/main.16eb7cb9.chunk.js"
+  },
+  {
+    "revision": "1eb29b79da48b124f7b5",
+    "url": "/static/js/runtime-main.8ad5dc57.js"
+  },
+  {
+    "revision": "cb0ac860078b0d0d1508455fed2f301a",
+    "url": "/static/media/assent.cb0ac860.png"
+  },
+  {
+    "revision": "9da95b39594bac27c3bcaf76c1d723f5",
+    "url": "/static/media/bmo.9da95b39.png"
+  },
+  {
+    "revision": "ef57c83b5ccd8e5a2ffb0077258fa757",
+    "url": "/static/media/card.ef57c83b.png"
+  },
+  {
+    "revision": "372c614bfe7e6af529c1bea2721edf27",
+    "url": "/static/media/me.372c614b.jpg"
+  },
+  {
+    "revision": "077fc333d7f480c22042722466009216",
+    "url": "/static/media/meta.077fc333.png"
+  },
+  {
+    "revision": "b7601579eac9879c399c7548f43a6e5d",
+    "url": "/static/media/operate.b7601579.png"
+  },
+  {
+    "revision": "20651bee5bc6ad51b215e0ae042d5f42",
+    "url": "/static/media/pxl.20651bee.gif"
+  },
+  {
+    "revision": "c5d1473a244821858540a6f692e6acf5",
+    "url": "/static/media/recipy.c5d1473a.png"
+  },
+  {
+    "revision": "cfaf48bc8418ac60121b924ffcb6416d",
+    "url": "/static/media/resume.cfaf48bc.pdf"
+  },
+  {
+    "revision": "410664cca4e851265fe55b03f0eb8e5a",
+    "url": "/static/media/tesla.410664cc.png"
+  },
+  {
+    "revision": "e0081e6fb1dc3af71cdf59163357cfd0",
+    "url": "/static/media/website.e0081e6f.png"
+  }
+]);
